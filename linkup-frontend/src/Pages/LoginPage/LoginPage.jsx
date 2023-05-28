402d93c865cc00827c712d8b630b4fb8e3adddf8
Arpit <arpittyagirocks@gmail.com>
2023-05-28 18:15:27 +0530

    frontend design of login page is ready

--- linkup-frontend/src/Pages/LoginPage/LoginPage.jsx
+++ linkup-frontend/src/Pages/LoginPage/LoginPage.jsx
@@ -1,22 +1,30 @@
 import React from "react";
 import "./LoginPage.css";   
 import SidebarImage from "./SidebarImage.jpg";
+import googleicon from "../../Assets/google-icon.svg";
 
 export default function LoginPage(){
     return(
         <>
             <div className="login-page-outer">
-                <div className="form-outer">
-                    <div className="login-page-title">Welcome</div>
-                    <div>Email Address</div>
-                    <input type="text" placeholder="email@example.com"/>
-                    <div>Password</div>
-                    <input type="password" placeholder="*****************"/>
+                <div className="login-form-outer">
+                    <div className="login-page-title">Welcome back</div>
+
+                    <div className="label">Email Address</div>
+                    <input type="email" placeholder="youremail@example.com"/>
+
+                    <div className="label">Password</div>
+                    <input type="password" placeholder="••••••••••"/>
+
                     <div><input type="checkbox"/> Remember me</div>
-                    <button>Login</button>
-                    <div>dont have an account sign in</div>
-                    or
-                    <div>Login with Google</div>
+
+                    <input type="submit" className="login-btn" value="Login"/>
+                    <div>Don't have an account, sign in</div>
+                    <div className="or">or</div>
+                    <div className="google-login-btn">
+                        Login with Google
+                        <img src={googleicon} className="googleicon" alt="google-icon"/>
+                    </div>
 
                 </div>
                 <img src={SidebarImage} alt="Internet Error" className="sidebar"/>
--- linkup-frontend/src/index.js
+++ linkup-frontend/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import "./index.css"
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider>
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
 
