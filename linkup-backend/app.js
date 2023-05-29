9ae03315d15a571a2f3f79a10053ace13e19c50b
Arpit <arpittyagirocks@gmail.com>
2023-05-29 18:10:40 +0530

    fixing errors

--- linkup-backend/app.js
+++ linkup-backend/app.js
@@ -9,6 +9,14 @@ require('dotenv').config();
 app.use(cors()); // Add cors middleware
 
 const server = http.createServer(app);
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",
