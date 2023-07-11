b02ad16d6e0a1546e2fad06fd1b22da629fde73e
Deshy Dan <deshy733@gmail.com>
2023-07-11 20:44:20 +0200

    Added test for getting a user with an invalid email

--- linkup-backend/tests/intergration/user.test.js
+++ linkup-backend/tests/intergration/user.test.js
@@ -5,3 +5,71 @@ const bcrypt = require('bcrypt');
 
 const MONGO_URI = process.env.MONGO_URI;
 const DB_NAME = process.env.DB_NAME;
+describe('/auth', () => {
+    let server;
+    let db;
+    let connection;
+
+
+    beforeEach(async () => {
+        connection = await MongoClient.connect(MONGO_URI, { useUnifiedTopology: true });
+        db = connection.db(DB_NAME);
+        server = require('../../app');
+    })
+
+    afterEach(async () => {
+        server.close()
+        await connection.close();
+
+    })
+    describe("/getuser/:email", () => {
+        let fname;
+        let email;
+        let usercollection;
+
+        beforeEach(async () => {
+            fname = "joe";
+            email = "hello@gmail.com";
+            usercollection = db.collection('users');
+
+
+            const dummyUser = {
+                fname: "Dummy",
+                lname: "dummy",
+                email: "email@gmail.com",
+                hashedPassword: "dummy"
+            }
+            await usercollection.insertOne(dummyUser);
+        })
+
+
+        afterEach(async () => {
+            await usercollection.deleteMany({});
+        })
+
+        const exec = async () => {
+            return await request(server)
+                .post('/getuser/' + email)
+                .send()
+        }
+        it('should return 404 if the user is not found', async () => {
+            email = 'invalid@gmail.com';
+
+            const res = await exec();
+            expect(res.status).toBe(404)
+
+        })
+
+
+
+
+
+
+
+
+    })
+
+
+
+
+})
