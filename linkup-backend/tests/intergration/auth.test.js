43e8840d8d8c49b8f5ca68cd0996d555af16cdec
Deshy Dan <deshy733@gmail.com>
2023-07-11 18:19:23 +0200

    refactored testing logic

--- linkup-backend/tests/intergration/auth.test.js
+++ linkup-backend/tests/intergration/auth.test.js
@@ -1,6 +1,7 @@
 const request = require('supertest')
 const { MongoClient } = require('mongodb');
 require("dotenv").config({ path: "../../.env.test" });
+const bcrypt = require('bcrypt');
 
 const MONGO_URI = process.env.MONGO_URI;
 const DB_NAME = process.env.DB_NAME;
@@ -10,42 +11,88 @@ describe("/auth", () => {
     let db;
     let connection;
 
-  
-    beforeEach(async() => {
+
+    beforeEach(async () => {
         connection = await MongoClient.connect(MONGO_URI, { useUnifiedTopology: true });
         db = connection.db(DB_NAME);
         server = require('../../app');
     })
 
-    afterEach( async() => {
+    afterEach(async () => {
         server.close()
- await connection.close();
+        await connection.close();
     })
-    describe('/auth/signup', () => {
-        it("should return the dummy user we entered", async () => {
-            const dummyUser = await usercollection.findOne({fname:"Dummy"})
 
-            expect(dummyUser).toMatchObject({
+
+    describe('/auth/signup', () => {
+        let fname;
+        let email;
+        let usercollection;
+
+        beforeEach(async () => {
+            fname = "joe";
+            email = "hello@gmail.com";
+            usercollection = db.collection('users');
+
+
+            const dummyUser = {
                 fname: "Dummy",
-                lname: "EVes",
+                lname: "dummy",
                 email: "email@gmail.com",
+                hashedPassword: "dummy"
+            }
+            await usercollection.insertOne(dummyUser);
+        })
+        
+        afterEach(async () => {
+            await usercollection.deleteMany({});
+        })
+
+
+        const exec = async () => {
+            return await request(server).post('/auth/signup').send({
+                fname: fname,
+                lname: "Doe",
+                email: email,
                 password: "12345"
             })
+        }
 
-        })
+        
+
+        it('should return 400 if all the fields are not specified', async () => {
+            fname = '';
             const res = await exec()
+
+            expect(res.status).toBe(400)
+        })
 
         it('should return 409 if user with that email already exists', async () => {
             email = "email@gmail.com";
-
+         
             const res = await exec();
-
-            expect(res.body).toBe(409)
+            
+            expect(res.status).toBe(409)
 
         })
+        it('should return the dummy user if email already exists', async () => {
+            email = "email@gmail.com";
+
+            const res = await exec();
+            const user = await usercollection.findOne({ email: email })
+
+            expect(user).toHaveProperty('fname', "Dummy")
+            expect(user).toHaveProperty('lname', "dummy")
+            expect(user).toHaveProperty('email', "email@gmail.com")
+        })
+      
         it("should return 200 if the user is inserted into the database", async () => {
             const res = await exec()
-            const newUser = await usercollection.findOne({email:email})
+            const newUser = await usercollection.findOne({ email: email })
             expect(res.status).toBe(200);
             expect(newUser.email).toBe(email)
         })
+
+
+    })
+})
