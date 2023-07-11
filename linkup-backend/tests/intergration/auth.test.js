65696810d49eb04fd5eceff25050fc4bd87a48e0
Deshy Dan <deshy733@gmail.com>
2023-07-11 19:20:31 +0200

    Added test for tesing payload after logging in

--- linkup-backend/tests/intergration/auth.test.js
+++ linkup-backend/tests/intergration/auth.test.js
@@ -44,29 +44,22 @@ describe("/auth", () => {
             }
             await usercollection.insertOne(dummyUser);
         })
-        
-        
+
+
         afterEach(async () => {
             await usercollection.deleteMany({});
         })
 
-
-
-    
-
         const exec = async () => {
-            return await request(server).post('/auth/signup').send({
-                fname: fname,
-                lname: "Doe",
-                email: email,
-                password: "12345"
-            })
+            return await request(server)
+                .post('/auth/signup')
+                .send({
+                    fname: fname,
+                    lname: "Doe",
+                    email: email,
+                    password: "12345"
+                })
         }
-
-        
-        
-
-
 
         it('should return 400 if all the fields are not specified', async () => {
             fname = '';
@@ -107,52 +100,55 @@ describe("/auth", () => {
     describe('/auth/login', () => {
         let email;
         let password;
+        let usercollection;
 
-       
+
         beforeEach(async () => {
-      
-            email = "hello@gmail.com";
+
+            email = "email@gmail.com";
             password = 'dummy';
             usercollection = db.collection('users');
 
             const hashedPassword = await bcrypt.hash("dummy", 10)
             const dummyUser = {
-                fname: "Dummy",
-                lname: "dummy",
+                name: "Dummer User",
                 email: "email@gmail.com",
-                hashedPassword: hashedPassword
+                email_verified: false,
+                hashedPassword: hashedPassword,
+                picture: null,
+                friends: []
             }
             await usercollection.insertOne(dummyUser);
         })
-        
-        
+
+
         afterEach(async () => {
             await usercollection.deleteMany({});
         })
 
         const exec = async () => {
-             return await request(server)
-             .post('/auth/login')
-             .send({
-                email:email,
-                password:password
-             })
+            return await request(server)
+                .post('/auth/login')
+                .send({
+                    email: email,
+                    password: password
+                })
         }
 
-        it('should return 400 if email is not specified' , async () => {
-            email =''
+        it('should return 400 if email is not specified', async () => {
+            email = ''
 
             const res = await exec();
 
             expect(res.status).toBe(400)
         })
 
-        it('should return 400 if password is not specified' , async () => {
-            password =''
+        it('should return 400 if password is not specified', async () => {
+            password = ''
 
             const res = await exec();
             expect(res.status).toBe(400)
-        
+
         })
 
         it("should retun 404 is user with the email was not found", async () => {
@@ -160,6 +156,34 @@ describe("/auth", () => {
             const res = await exec();
             expect(res.status).toBe(404)
         })
+
+        it("should return 401 is the user entered a wrong password", async () => {
+            password = "wrong"
+            const res = await exec()
+            expect(res.status).toBe(401)
+        })
+
+        it("should return 200 is the user is succesfuly logged in", async () => {
+            const res = await exec();
+            expect(res.status).toBe(200)
+        })
+
+        it("should return a payload when user logs in", async () => {
+
+            const res = await exec();
+            const user = await usercollection.findOne({email:email})
+
+            const payload = res._body['payload'];
+            expect(payload).toHaveProperty('name',user.name)
+            expect(payload).toHaveProperty('email', user.email)
+            expect(payload).toHaveProperty('picture', user.picture)
+            expect(payload).toHaveProperty('friends', user.friends)
+
+
+
+        })
+
+
 
 
     })
