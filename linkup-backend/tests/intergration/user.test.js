3171387d980ad51793dcdbe2fc96108b04c78a97
Deshy Dan <deshy733@gmail.com>
2023-07-11 21:31:47 +0200

    Added test for getting all users in the database

--- linkup-backend/tests/intergration/user.test.js
+++ linkup-backend/tests/intergration/user.test.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcrypt');
 
 const MONGO_URI = process.env.MONGO_URI;
 const DB_NAME = process.env.DB_NAME;
-describe('/auth', () => {
+describe('/user', () => {
     let server;
     let db;
     let connection;
@@ -22,14 +22,14 @@ describe('/auth', () => {
         await connection.close();
 
     })
-    describe("/getuser/:email", () => {
+    describe("GET /user/:email", () => {
         let fname;
         let email;
         let usercollection;
 
         beforeEach(async () => {
             fname = "joe";
-            email = "hello@gmail.com";
+            email = "email@gmail.com";
             usercollection = db.collection('users');
 
 
@@ -49,7 +49,7 @@ describe('/auth', () => {
 
         const exec = async () => {
             return await request(server)
-                .get('/getuser/' + email)
+                .get('/user/getuser/' + email)
                 .send()
         }
         it('should return 404 if the user is not found', async () => {
@@ -59,15 +59,83 @@ describe('/auth', () => {
             expect(res.status).toBe(404)
 
         })
-        
+
         it('should return 200 is the user with the email was found', async () => {
-            
+
             const res = await exec();
             expect(res.status).toBe(200)
         })
 
     })
 
+    describe("GET /user/getallusers", () => {
+
+        let usercollection;
+        let users;
+
+        beforeEach(async () => {
+
+            usercollection = db.collection('users');
+
+            users = [
+                {
+                    name: "Dummy",
+                    email: "email@gmail.com",
+                    hashedPassword: "dummy"
+                },
+                {
+                    name: "DummyOne",
+                    email: "email1@gmail.com",
+                    hashedPassword: "dummy"
+                },
+                {
+                    name: "DummyTwo",
+                    email: "email2@gmail.com",
+                    hashedPassword: "dummy"
+                },
+                {
+                    name: "DummyThree",
+                    email: "email3@gmail.com",
+                    hashedPassword: "dummy"
+                }
+            ]
+
+            await usercollection.insertMany(users);
+        })
+
+
+        afterEach(async () => {
+            await usercollection.deleteMany({});
+        })
+
+        const exec = async () => {
+            return await request(server)
+                .get('/user/getallusers/')
+                .send()
+        }
+
+        it("should return all the users", async () => {
+            const res = await exec();
+            const body = res.body;
+
+
+            let isValid = true;
+            let i = 0;
+            for (i = 0; i < 4; i++) {
+                if (users[i]['name'] !== body[i]['name']
+                    && users[i]['email'] !== body[i]['email']
+                    ){
+                    isValid = false;
+                    break;
+                }
+            }
+
+            expect(isValid).toBe(true)
+
+
+        })
+
+    })
 
 
 
