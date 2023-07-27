a31c52087f49f485a86b9e51fdd03fcee74e1038
Rohit Kharche <111701144+rohitrokz29@users.noreply.github.com>
2023-07-27 21:57:13 +0530

    added Lazy Loading in frontend
    Added React Lazy loaders and fallback component

---
+++ linkup-frontend/src/Components/Loaders/LazyLoader.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './lazyloader.css'
+
+const LazyLoader = () => {
+
+    return (
+        <div role='fallback'
+            className=" main-fallback center-item">
+            <div className="outer-roll center-item ">
+            </div>
+        </div>
+    )
+}
+
+export default LazyLoader; 
